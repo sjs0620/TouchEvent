083e2436b9dbfdb9fc70e48c5f07a3e8cbcc3342
신재순 <sjslsm@gmail.com>
2018-10-23 14:26:27 +0900

    Initial commit

---
+++ app/src/main/java/com/sjs/edu/touchevent/MainActivity.java
@@ -0,0 +1,54 @@
+package com.sjs.edu.touchevent;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+class MyView extends  View{
+    int key;
+    String str;
+    int x=100,y=100;
+    public MyView(Context context){
+        super(context);
+        setBackgroundColor(Color.YELLOW);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        x = (int)event.getX();
+        y = (int)event.getY();
+        if(event.getAction() == MotionEvent.ACTION_DOWN)
+            str = "액션다운";
+        if(event.getAction() == MotionEvent.ACTION_MOVE)
+            str = "액션무브";
+        if(event.getAction() == MotionEvent.ACTION_UP)
+            str = "액션업";
+        invalidate();
+        return  true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setTextSize(50);
+        canvas.drawCircle(x,y,30, paint);
+        canvas.drawText("("+x+","+y+")액션종류: "+str,2,100,paint);
+        super.onDraw(canvas);
+    }
+}
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MyView myView = new MyView(this);
+        setContentView(myView);
+    }
+}
